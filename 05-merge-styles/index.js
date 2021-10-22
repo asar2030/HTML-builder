bb096f5f10adbaeb4f1f98ad57189203001eb3a6
Asar Akhmetbekov <you@example.com>
2021-10-22 21:39:47 +0600

    feat: 5/6

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs').promises;
+
+
+async function mergeStyles() {
+  try {
+    const files = await readdir(path.join(__dirname, 'styles'), {
+      withFileTypes: true,
+    });
+    for (const file of files) {
+      if (file.isFile()) {
+        const fileType = path.extname(`${path.join(__dirname, `styles/${file.name}`)}`).slice(1);
+        if (fileType == 'css') {
+          const filePath = path.join(__dirname, `/styles/${file.name}`);
+          fs.readFile(filePath, 'utf-8', function(error, data) {
+            if(error) throw error;
+            fs.writeFile(path.join(__dirname, 'styles', 'bundle.css'), data,
+              (err) => {
+                if (err) throw err;
+              }
+            );
+          });
+        }
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+}
+mergeStyles();
