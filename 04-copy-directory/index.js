a94061f122044bee1ec998a0c0f67c90afacffde
Asar Akhmetbekov <you@example.com>
2021-10-22 19:42:27 +0600

    feat: 4/6

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require('fs');
+const path = require('path');
+const { readdir } = require('fs').promises;
+const { copyFile } = require('fs/promises');
 
+async function copy() {
+  fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, err => {
+    if (err) throw err;
+    console.log('Папка была создана');
+  });
+
+  try {
+    const files = await readdir(path.join(__dirname, 'files'), {
+      withFileTypes: true,
+    });
+    for (const file of files) {
+      let srcPath = path.join(__dirname, 'files', file.name);
+      let destPath = path.join(__dirname,'files-copy', file.name);
+      if (file.isFile()) {
+        await copyFile(srcPath, destPath);
+      }
+    }
+  } catch (err) {
+    console.error(err);
+  }
+
+}
+copy();
